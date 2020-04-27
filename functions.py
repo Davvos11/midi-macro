c21dff463ffd0436d4e3ce58e28b3971f87eeb90
Davvos11 <vosdavid2@gmail.com>
2020-04-28 00:15:47 +0200

    added functions

--- functions.py
+++ functions.py
@@ -1,11 +1,25 @@
 from midi import Midi
+from pulseaudio import PulseControl
+from mpris import MprisControl
 
 
-def on_note(values):
-    print(values)
-    print('test1234')
+def knob_1_1(values):
+    with PulseControl() as pulse:
+        pulse.set_stream_volume('AudioStream', values[1]/127)
+
+
+def knob_1_8(values):
+    with PulseControl() as pulse:
+        pulse.set_stream_volume('Spotify', values[1]/127)
+
+
+def pad_1_4(values):
+    with MprisControl() as mpris:
+        mpris.play_pause_auto()
 
 
 class Functions:
     def __init__(self, midi: Midi):
-        midi.add_event(on_note, 1, midi.Type.NOTE_ON)
+        midi.add_event(knob_1_1, 1, midi.Type.CC, 1)
+        midi.add_event(knob_1_8, 1, midi.Type.CC, 8)
+        midi.add_event(pad_1_4, 1, midi.Type.NOTE_ON, 39)
