d7c3ab3b43ad787ae91b91acf8fdc16c3e597291
Davvos11 <vosdavid2@gmail.com>
2020-04-27 12:38:42 +0200

    added pulseaudio backend

---
+++ pulseaudio.py
@@ -0,0 +1,19 @@
+import pulsectl
+
+
+class PulseControl:
+    def __init__(self, name: str = "Python control"):
+        self.pulse = pulsectl.Pulse(name)
+
+    def get_stream_list(self) -> [pulsectl.PulseSinkInfo]:
+        return self.pulse.sink_input_list()
+
+    def get_stream(self, name: str) -> pulsectl.PulseSinkInfo:
+        for stream in self.get_stream_list():
+            if stream.name == name:
+                return stream
+
+    def set_stream_volume(self, name: str, level: float) -> None:
+        stream = self.get_stream(name)
+        if stream is not None:
+            self.pulse.volume_set_all_chans(stream, level)
